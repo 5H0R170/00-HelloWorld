643409529e51ec62b9e18ab84d0a333ca1a156b4
Nikita Chernov <chernov.nikita.91@gmail.com>
2022-01-26 18:18:04 +0700

    Добавьте файлы проекта.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+﻿// Эта программа выводит на экран сообщение "Hello, World!"
+#include "../../std_lib_facilities.h"
+
+int main() // Программы на C++ начинаются с выполнения функции main
+{
+    cout << "Hello, World!\n"; // Вывод строки "Hello, World!"
+    keep_window_open(); // Ожидание ввода символа
+    return 0;
+}
